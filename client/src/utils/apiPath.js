c7f467c56ff2e218289fc539b590c3e7fe98ffda
Sheikh-Tafsir <190041130tafsir@gmail.com>
2024-06-30 18:52:24 +0600

    need to think how to add github projects

--- client/src/utils/apiPath.js
+++ client/src/utils/apiPath.js
@@ -1,3 +1,3 @@
-export const apiPath = 'http://192.168.1.3:8001';
-// export const apiPath = 'https://portle-api.vercel.app';
+//export const apiPath = 'http://192.168.1.3:8001';
+export const apiPath = 'https://portle-api.vercel.app';
 // export const apiPath = 'http://192.168.17.138:8001';
